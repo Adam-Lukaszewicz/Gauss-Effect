b48e0c41333226bce5737215fbe43e9c5e416602
Adam-Lukaszewicz <jakub.adam20@gmail.com>
2023-11-12 13:45:26 +0100

    Even workthread

--- Gauss5Main/Gauss5CPP/traansformationCPP.cpp
+++ Gauss5Main/Gauss5CPP/traansformationCPP.cpp
@@ -5,8 +5,11 @@
 int basicTrf(uint8_t* beginPtr, uint8_t* endPtr) {
 	while (beginPtr != endPtr) {
 		*(beginPtr) += 100;
-		if (*(beginPtr) > 255) *(beginPtr) -= 255;
 		beginPtr++;
 	}
+	return 1;
+}
+
+int gaussTrf(uint8_t* beginPtr, uint8_t* endPtr) {
 	return 1;
 }
--- Gauss5Main/Gauss5Main/Gauss5Main.cpp
+++ Gauss5Main/Gauss5Main/Gauss5Main.cpp
@@ -12,22 +12,42 @@ extern "C" int basicTrfAsm(uint8_t* beginPtr, uint8_t * endPtr);
 
 int main()
 {
-    int threads = 4;
+    int threads = 5;
 
 
     BMP test("t2_24.bmp", threads);
     BMP testC("t2_24.bmp", threads);
-    while (test.data.size() % threads != 0) threads--; //Pętla obcinająca ilość wątków do takiej, której uda się równo obsłużyć
+    //while (test.data.size() % threads != 0) threads--; //Pętla obcinająca ilość wątków do takiej, której uda się równo obsłużyć
+    int overflow = test.data.size() % threads;
+    int usedOverflow = 0;
+    if (overflow != 0) std::cout << "Nierowne thready\n";
     Timer::start();
     std::vector<std::jthread> handler;
     for (int i = 0; i < threads; i++) {
-        handler.push_back(std::jthread(basicTrfAsm, test.data.data() + i * test.data.size()/threads, (test.data.data() + (i+1) * test.data.size()/threads) - 1));
+        if (overflow != 0) {
+            handler.push_back(std::jthread(basicTrfAsm, test.data.data() + i * test.data.size() / threads + usedOverflow, (test.data.data() + (i + 1) * test.data.size() / threads) + usedOverflow));
+            overflow--;
+            usedOverflow++;
+        } 
+        else {
+            handler.push_back(std::jthread(basicTrfAsm, test.data.data() + i * test.data.size() / threads + usedOverflow, (test.data.data() + (i + 1) * test.data.size() / threads) - 1 + usedOverflow));
+        }
     }
     //basicTrfAsm(test.data.data(), test.data.data() + test.data.size() - 1);
     Timer::stop();
+    int overflowC = testC.data.size() % threads;
+    int usedOverflowC = 0;
+    if (overflowC != 0) std::cout << "Nierowne thready\n";
     Timer::start();
     for (int i = 0; i < threads; i++) {
-        handler.push_back(std::jthread(basicTrf, testC.data.data() + i * testC.data.size() / threads, (testC.data.data() + (i + 1) * testC.data.size() / threads) - 1));
+        if (overflowC != 0) {
+            handler.push_back(std::jthread(basicTrf, testC.data.data() + i * testC.data.size() / threads + usedOverflowC, (testC.data.data() + (i + 1) * testC.data.size() / threads) + usedOverflowC));
+            overflowC--;
+            usedOverflowC++;
+        }
+        else {
+            handler.push_back(std::jthread(basicTrf, testC.data.data() + i * testC.data.size() / threads + usedOverflowC, (testC.data.data() + (i + 1) * testC.data.size() / threads) - 1 + usedOverflowC));
+        }
     }
     //basicTrf(testC.data.data(), testC.data.data() + testC.data.size() - 1);
     Timer::stop();
